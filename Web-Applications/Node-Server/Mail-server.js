4a38573dc051bfbfc0f709e350e213ff6be11e8c
ehoelzl <edoardo.holzl@epfl.ch>
2018-04-28 15:49:54 +0200

    Corection

--- Web-Applications/Node-Server/Mail-server.js
+++ Web-Applications/Node-Server/Mail-server.js
@@ -21,7 +21,7 @@ module.exports = {
             from: '"DNA" <eth.notary@gmail.com>',
             to: user,
             subject: 'Signature',
-            text: "Dear user, \n\n please find attached your signature for your timestamped document with hash 0x" + hash[:64] + ". \n Keep it safe. \n\n" +
+            text: "Dear user, \n\n please find attached your signature for your timestamped document with hash 0x" + hash.substr(0,64) + ". \n Keep it safe. \n\n" +
             "This is an automatic email, please do not answer. \n\n The DNA team \n\n",
             attachments: [{
                 filename: 'signature.json',
